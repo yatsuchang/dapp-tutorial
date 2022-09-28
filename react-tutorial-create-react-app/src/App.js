842d07afd6166600327833c1bb6ba04c53f31625
Chang Thomas <thomaschang@ChangdeMacBook-Pro.local>
2022-09-28 17:49:36 +0800

    add Metamask checking & useState

--- react-tutorial-create-react-app/src/App.js
+++ react-tutorial-create-react-app/src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+import {useState} from "react";
 import './App.css';
 
-function App() {
+// Add function
+const isMetaMaskInstalled = () => {
+  const { ethereum } = window;
+  console.log('ethereum:', ethereum)
+  //web3js = new Web3(Web3.currentProvider);
+  //console.log('web3js:', web3js)
+  if(typeof ethereum === 'undefined') {
+      return false;
+  }
+  return Boolean(ethereum && ethereum.isMetaMask);
+}
+
+// Make function App() {} to an arrow function component
+const App = () => {
+  const [value, setValue] = useState(1);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello, React!</h1>
+      <p>You can type web3 or ethereum in developer console, it prints a Proxy object</p>
+      <p>Metamask: { isMetaMaskInstalled() ? "Installed" : "Not Found" }</p>
+
+      <div>
+        <p>{value}</p>
+        <button onClick={() => setValue((value + 1))}>Increment Value</button>
+      </div>
     </div>
   );
 }
