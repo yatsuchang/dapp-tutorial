3cd812cc569fb681c136b47434f8d2e2cdd7603b
Chang Thomas <thomaschang@ChangdeMacBook-Pro.local>
2022-09-30 15:13:31 +0800

    Add switch network function ----- Use library.provider to send request to Web3Provider

--- frontend/react-tutorial-typescript/src/App.tsx
+++ frontend/react-tutorial-typescript/src/App.tsx
@@ -18,18 +18,52 @@ const Injected = new InjectedConnector({
 function App() {
   /*  active: boolean indicating connection to user’s wallet
       account: connected user's public wallet address
-      chainId: chain id of the currently connected network */
-  const { chainId, account, active, activate, deactivate } = useWeb3React()
+      chainId: chain id of the currently connected network
+      library: to add/switch networks, must make a request to Web3Provider */
+  const { chainId, account, active, activate, deactivate, library } = useWeb3React()
+
+  // example of switching or adding network with Harmony Mainnet
+  const switchNetwork = async () => {
+    // https://rpc.info/ or https://docs.rango.exchange/integrations
+    try {
+      await library.provider.request({
+        method: "wallet_switchEthereumChain",
+        params: [{ chainId: "0x63564c40" }],
+      });
+    } catch (switchError: any) {
+      // 4902 error code indicates the chain is missing on the wallet
+      if (switchError.code === 4902) {
+        try {
+          await library.provider.request({
+            method: "wallet_addEthereumChain",
+            params: [
+              {
+                chainId: "0x63564c40",
+                rpcUrls: ["https://api.harmony.one"],
+                chainName: "Harmony Mainnet",
+                nativeCurrency: { name: "ONE", decimals: 18, symbol: "ONE" },
+                blockExplorerUrls: ["https://explorer.harmony.one"],
+                iconUrls: ["https://harmonynews.one/wp-content/uploads/2019/11/slfdjs.png"]
+              }
+            ],
+          });
+        } catch (error) {
+          console.error(error)
+        }
+      }
+    }
+  };
 
   return (
     <div className="App">
-      <div>Connection Status: {active}</div>
-      <div>{Account: {account}</div>
-      <div>{Network ID: {chainId}</div>
+      <div>Connection Status: {active ? 'already' : 'not'} connected</div>
+      <div>Account: {account}</div>
+      <div>Network ID: {chainId}</div>
 
       <button onClick={() => { activate(Injected) }}>Metamask</button>
 
       <button onClick={deactivate}>Disconnect</button>
+      <button onClick={switchNetwork}>Switch to Harmony Mainnet</button>
     </div>
   );
 }
