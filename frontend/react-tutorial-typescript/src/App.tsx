2cbf3533007f9edf7ee01615cb32fe5f5fb3aefc
Chang Thomas <thomaschang@ChangdeMacBook-Pro.local>
2022-09-30 14:07:58 +0800

    Add injected wallet support ----- index.js: import/setup Web3ReactProvider App.tsx: add injected wallet connectors

--- frontend/react-tutorial-typescript/src/App.tsx
+++ frontend/react-tutorial-typescript/src/App.tsx
@@ -1,24 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
+import { useWeb3React } from '@web3-react/core'; // hook for wallet connection events
+import { InjectedConnector } from "@web3-react/injected-connector";// Injected (e.g. Metamask, safepal, trustwallet)
+
 import './App.css';
 
+const Injected = new InjectedConnector({
+  supportedChainIds: [
+      1, // Mainet
+      3, // Ropsten
+      4, // Rinkeby
+      5, // Goerli
+      42, // Kovan
+      56, // BSC
+  ],
+})
+
 function App() {
+  /*  active: boolean indicating connection to user’s wallet
+      account: connected user's public wallet address
+      chainId: chain id of the currently connected network */
+  const { chainId, account, active, activate, deactivate } = useWeb3React()
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>Connection Status: {active}</div>
+      <div>{Account: {account}</div>
+      <div>{Network ID: {chainId}</div>
+
+      <button onClick={() => { activate(Injected) }}>Metamask</button>
+
+      <button onClick={deactivate}>Disconnect</button>
     </div>
   );
 }
--- frontend/react-tutorial-typescript/src/index.tsx
+++ frontend/react-tutorial-typescript/src/index.tsx
@@ -1,15 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Web3ReactProvider } from '@web3-react/core'
+import { Web3Provider } from "@ethersproject/providers";
+
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+function getLibrary(provider: any): Web3Provider {
+  const library = new Web3Provider(provider)
+  library.pollingInterval = 12000
+  return library
+}
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Web3ReactProvider getLibrary={getLibrary}>
+      <App />
+    </Web3ReactProvider>
   </React.StrictMode>
 );
 
