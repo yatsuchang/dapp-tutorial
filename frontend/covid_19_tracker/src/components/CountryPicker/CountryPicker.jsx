ba11bd6e5801e44630d5d69240add240c7fbe03a
Chang Thomas <thomaschang@ChangdeMacBook-Pro.local>
2022-10-08 01:27:58 +0800

    covid-19: init commit

---
+++ frontend/covid_19_tracker/src/components/CountryPicker/CountryPicker.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {useState, useEffect} from "react";
+
+const CountryPicker = () => {
+  
+    return (
+      <h1>CountryPicker</h1>
+    )
+  }
+  export default CountryPicker;
