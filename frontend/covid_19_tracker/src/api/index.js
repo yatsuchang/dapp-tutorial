5a5b7f1952c18dcabb7febee957208544c1ead92
Chang Thomas <thomaschang@ChangdeMacBook-Pro.local>
2022-10-08 17:09:12 +0800

    covid-19: add chart

--- frontend/covid_19_tracker/src/api/index.js
+++ frontend/covid_19_tracker/src/api/index.js
@@ -10,3 +10,18 @@ export const fetchData = async () => {
 
   }
 }
+
+export const fetchDailyData = async () => {
+  try {
+    const { data } = await axios.get(`${url}/daily`);
+    const modifiedData = data.map((dailyData) => ({
+      confirmed: dailyData.confirmed.total,
+      deaths: dailyData.deaths.total,
+      date: dailyData.reportDate,
+    }));
+    console.log('debug0 modifiedData:', modifiedData);
+    return modifiedData;
+  } catch (error) {
+
+  }
+}
--- frontend/covid_19_tracker/src/components/Chart/Chart.jsx
+++ frontend/covid_19_tracker/src/components/Chart/Chart.jsx
@@ -1,10 +1,58 @@
-import React from 'react';
-import {useState, useEffect} from "react";
+import React, {useState, useEffect}  from 'react';
+
+
+
+import { Line, Bar } from 'react-chartjs-2';
+import { fetchDailyData } from '../../api';
+
+import styles from './Chart.module.css';
+
+// https://react-chartjs-2.js.org/faq/registered-scale
+import { Chart as cht, registerables } from 'chart.js';
+cht.register(...registerables);
 
 const Chart = () => {
-  
-    return (
-      <h1>Chart</h1>
-    )
-  }
-  export default Chart;
+  const [dailyData, setDailyData] = useState([]);
+
+  useEffect(() => {
+    // a tricky when calling async inside useEffect
+    const fetchAPI = async () => {
+      const initDailyData = await fetchDailyData();
+      console.log('debug1 dailyData:', initDailyData);
+      setDailyData(initDailyData);
+    }
+    fetchAPI();
+  }, []);
+
+  // global data drawing by line chart
+  const lineChart = (
+    dailyData.length
+      ? (
+        <Line
+          data={{
+            labels: dailyData.map(({ date }) => date),
+            datasets: [{
+              data: dailyData.map(({ confirmed }) => confirmed),
+              label: 'Infected',
+              borderColor: '#3333ff',
+              fill: true,
+            }, {
+              data: dailyData.map(({ deaths }) => deaths),
+              label: 'Deaths',
+              borderColor: 'red',
+              backgroundColor: 'rgba(255, 0, 0, 0.5)',
+              fill: true,
+            }],
+          }}
+        />
+      ) : null
+  );
+
+  return (
+    <div className={styles.container}>
+      {lineChart}
+    </div>
+  )
+}
+
+export default Chart;
